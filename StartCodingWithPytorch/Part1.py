a3f4d08be45ef9f641e5929926b628a84adaf2d7
SHAYAN <shayhanww@gmail.com>
2023-03-21 18:04:44 +0330

    Tensors

---
+++ StartCodingWithPytorch/Part1.py
@@ -0,0 +1,15 @@
+import torch
+#scaler
+scaler = torch.tensor(2.5)
+print(scaler)
+
+#vextor
+vector = torch.tensor([1,2,3,4,5,6])
+
+#matrix
+matrix = torch.tensor([[1,2],[1,2],[2,3]])
+col_vec = torch.tensor([[1],[2]])
+
+#type
+
+
